b0b781ad6749ca8ea18ff8a67cd7156340f50d24
Salatiel Ferreira <salatielmf49@gmail.com>
2023-11-25 23:47:19 -0300

    slider added

--- script.js
+++ script.js
@@ -1,15 +1,55 @@
+//remove opening screen
 setTimeout(() => {
-  let openingScreen = document.querySelector('openingScreen')
+  let openingScreen = document.getElementById('openingScreen')
   openingScreen.style.display = 'none'
 }, 4000)
 
+//mobile menu
 function openAndClose(command) {
   let menu = document.getElementById('menu')
   if (command == 'close') {
-    //menu.style.visibility = 'hidden'
     menu.style.display = 'none'
   } else {
-    //menu.style.visibility = 'visible'
     menu.style.display = 'block'
+  }
+}
+
+//carousel
+let indexCounter = 0
+setInterval(() => {
+  carousel(indexCounter)
+  indexCounter++
+  if (indexCounter >= 3) {
+    indexCounter = 0
+  }
+}, 5000)
+
+function carousel(index) {
+  let input
+  switch (index) {
+    case 0:
+      document
+        .getElementsByClassName('carousel-images')
+        .item(0).style.backgroundImage = 'url(assets/slider-1.jpg)'
+      input = document.querySelectorAll('.carousel input').item(0)
+      input.checked = true
+      indexCounter = 0
+      break
+    case 1:
+      document
+        .getElementsByClassName('carousel-images')
+        .item(0).style.backgroundImage = 'url(assets/slider-2.jpg)'
+      input = document.querySelectorAll('.carousel input').item(1)
+      input.checked = true
+      indexCounter = 1
+      break
+    case 2:
+      document
+        .getElementsByClassName('carousel-images')
+        .item(0).style.backgroundImage = 'url(assets/slider-3.jpg)'
+      input = document.querySelectorAll('.carousel input').item(2)
+      input.checked = true
+      indexCounter = 2
+      break
   }
 }
